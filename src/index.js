078b2040c523c4c9f671e80fd087192d4304890b
Vitaly <ivanov.arduan@yandex.ru>
2022-02-14 19:06:34 +0300

    Пробую разные теги.

--- src/index.js
+++ src/index.js
@@ -4,20 +4,7 @@ import App from './App';
 
 
 ReactDOM.render(
-
-    <div>
-        <lu>
-            <li>
-                <h3>Иванов Виталий</h3>
-                <h3>Иванова Светлана</h3>
-                <h3>Доброхвалов Андрей</h3>
-            </li>
-
-
-        </lu>
-        <h1>Приложение работает</h1>
-        <button>Кнопка</button>
-    </div>,
+<App/>,
 
   document.getElementById('root')
 );
