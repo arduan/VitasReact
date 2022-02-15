c43e0d91cb673eb80d18df05e4748197a1c7a8f3
Vitaly <ivanov.arduan@yandex.ru>
2022-02-15 11:21:44 +0300

    Пробую разные функции

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 
 
 function App() {
-  let likes = 0;
+  const [likes, setLikes] = useState(0)
+  const [value, setValue] = useState('Текст в инпуте')
 
   function increment(){
-    likes += 1;
-    console.log(likes)
+    setLikes(likes +1)
   }
-    function decrement(){
-        likes -= 1;
-        console.log(likes)
-    }
+  function decrement(){
+      setLikes(likes -1)
+  }
   return (
     <div className="App">
+
+        <h1>{value}</h1>
+        <input type="text"
+               value={value}
+        onChange={event => setValue(event.target.value)}
+        />
       <h1>{likes}</h1>
       <button onClick={() => increment()}>Increment</button>
       <button onClick={() => decrement()}>Decrement</button>
